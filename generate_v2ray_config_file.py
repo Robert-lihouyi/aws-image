53eb10238a186c5d336296c4ea5af1ce2417627d
Robert <lihouyi228@Hotmail.com>
2021-04-21 15:47:24 +0800

    Update generate_v2ray_config_file.py

--- generate_v2ray_config_file.py
+++ generate_v2ray_config_file.py
@@ -1,43 +1,46 @@
 #!/usr/bin/python3
 
-import sys, getopt
+import sys
+import getopt
 import json
 import requests
-import os
+import subprocess
+
 
 def AssemblingV2RayConfigObject(inbounds_port, socks_user, socks_pass):
     return json.dumps({
-            "inbounds": [
-                    {
-                        "port": inbounds_port,
-                        "listen": "0.0.0.0",
-                        "tag": "socks-inbound",
-                        "protocol": "socks",
-                        "settings": {
-                                "auth": "password",
-                                "accounts": [
-                                        {
-                                            "user": socks_user,
-                                            "pass": socks_pass
-                                            }
-                                    ],
-                                "udp": False,
-                                "ip": "127.0.0.1"
-                            },
-                        "sniffing": {
-                            "enabled": True,
-                            "destOverride": ["http", "tls"]
-                            }
-                        }
-                ],
-            "outbounds": [
-                    {
-                        "protocol": "freedom",
-                        "settings": {},
-                        "tag": "direct"
-                        }
-                ]
-        }, indent=4)
+        "inbounds": [
+            {
+                "port": inbounds_port,
+                "listen": "0.0.0.0",
+                "tag": "socks-inbound",
+                "protocol": "socks",
+                "settings": {
+                            "auth": "password",
+                            "accounts": [
+                                    {
+                                        "user": socks_user,
+                                        "pass": socks_pass
+                                    }
+                            ],
+                    "udp": False,
+                    "ip": "127.0.0.1"
+                },
+                "sniffing": {
+                    "enabled": True,
+                    "destOverride": ["http", "tls"]
+                }
+            }
+        ],
+        "outbounds": [
+            {
+                "protocol": "freedom",
+                "settings": {},
+                "tag": "direct"
+            }
+        ]
+    }, indent=4)
+
 
 if __name__ == "__main__":
     write_file_path = ""
@@ -57,8 +60,11 @@ if __name__ == "__main__":
         sys.exit(2)
     # TODO: Obtains socks authenticate info
 
-    userData = json.loads(requests.get('http://169.254.169.254/latest/user-data/').text)
-    v2ray_config_file_content = AssemblingV2RayConfigObject(userData['port'], userData['user'], userData['pass'])
+    userData = json.loads(requests.get(
+        'http://169.254.169.254/latest/user-data').text)
+    v2ray_config_file_content = AssemblingV2RayConfigObject(
+        userData['port'], userData['user'], userData['pass'])
     with open(write_file_path, "w") as config_file:
         config_file.write(v2ray_config_file_content)
-    os.popen('/usr/bin/v2ray --config /etc/v2ray.json')
+    subprocess.call(["/usr/bin/v2ray", "--config",
+                    "/etc/v2ray.json"], shell=False)
